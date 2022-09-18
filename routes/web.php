e1135e612bc195c161d4b91701269176971011ee
Alex Labanino <ghostrider6610@gmail.com>
2022-09-17 22:56:03 -0400

    Setting up laravel debugbar

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $name = "Code For Designers";
+
+    return view('welcome', [
+        'name' => $name
+    ]);
 });
