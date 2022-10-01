8a856f62067d34f303cec112b1b759a4dfdfec1d
Alex Labanino <ghostrider6610@gmail.com>
2022-10-01 16:54:17 -0400

    Image upload in Laravel

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -28,7 +28,7 @@
 
         <div class="block lg:flex flex-row">
             <div class="basis-9/12 text-center sm:block sm:text-left">
-                <span class="text-left sm:text-center sm:text-left sm:inline block text-gray-900 pb-10 sm:pt-0 pt-0 sm:pt-10 pl-0 sm:pl-4 -mt-8 sm:-mt-0">
+                <span class="text-left sm:text-center sm:inline block text-gray-900 pb-10 sm:pt-0 pt-0 pl-0 sm:pl-4 -mt-8 sm:-mt-0">
                     Made by:
                     <a
                         href=""
